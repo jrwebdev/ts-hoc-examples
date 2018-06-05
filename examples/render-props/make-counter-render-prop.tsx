2b2125b4b367fff35ceb48608a29a304323e1f32
jrwebdev <web@jravenscroft.co.uk>
2018-06-05 17:38:20 +1200

    Render prop example

--- examples/render-props/make-counter-render-prop.tsx
+++ examples/render-props/make-counter-render-prop.tsx
@@ -50,6 +50,55 @@ class MakeCounter extends React.Component<MakeCounterProps, MakeCounterState> {
 
 export default MakeCounter;
 
+// interface CounterProps {
+//   style: React.CSSProperties;
+//   minValue?: number;
+//   maxValue?: number;
+// }
+
+// const Counter = (props: CounterProps) => (
+//   <MakeCounter minValue={props.minValue} maxValue={props.maxValue}>
+//     {injectedProps => (
+//       <div style={props.style}>
+//         <button onClick={injectedProps.onDecrement}> - </button>
+//         {injectedProps.value}
+//         <button onClick={injectedProps.onIncrement}> + </button>
+//       </div>
+//     )}
+//   </MakeCounter>
+// );
+
+// interface CounterProps {
+//   style: React.CSSProperties;
+//   value: number;
+//   minCounterValue?: number;
+//   maxCounterValue?: number;
+// }
+
+// const Counter = (props: CounterProps) => (
+//   <MakeCounter
+//     minValue={props.minCounterValue}
+//     maxValue={props.maxCounterValue}
+//   >
+//     {injectedProps => (
+//       <div>
+//         <div>Some other value: {props.value}</div>
+//         <div style={props.style}>
+//           <button onClick={injectedProps.onDecrement}> - </button>
+//           {injectedProps.value}
+//           <button onClick={injectedProps.onIncrement}> + </button>
+//         </div>
+//         {props.minCounterValue !== undefined ? (
+//           <div>Min value - {props.minCounterValue}</div>
+//         ) : null}
+//         {props.maxCounterValue !== undefined ? (
+//           <div>Min value - {props.maxCounterValue}</div>
+//         ) : null}
+//       </div>
+//     )}
+//   </MakeCounter>
+// );
+
 interface CounterProps extends InjectedCounterProps {
   style: React.CSSProperties;
 }
@@ -60,4 +109,19 @@ const Counter = (props: CounterProps) => (
     {props.value}
     <button onClick={props.onIncrement}> + </button>
   </div>
+);
+
+interface WrappedCounterProps extends CounterProps {
+  minValue?: number;
+  maxValue?: number;
+}
+
+const WrappedCounter = ({
+  minValue,
+  maxValue,
+  ...props
+}: WrappedCounterProps) => (
+  <MakeCounter minValue={minValue} maxValue={maxValue}>
+    {injectedProps => <Counter {...props} {...injectedProps} />}
+  </MakeCounter>
 );
